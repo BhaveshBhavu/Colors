fb0593c263abf6030593ba093c1d57eae958cd0b
bhaveshabcd1234bhavu@gmail.com <bhaveshabcd1234bhavu@gmail.com>
2022-06-25 18:24:09 +0530

    barn

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log('Hello World!');
+var y = document.getElementById('h') ;
+var u = document.getElementById('oi') ;
+var o = document.getElementById('DS') ;
+var p = document.getElementById('oo') ;
+var oo = document.getElementById('oh') ;
+var pm = document.getElementById('colorbox') ;
+var oto = document.getElementById('tex') ;
+function ko(){
+ 
+  y.classList.toggle('gfr');
+  u.classList.toggle('li');
+  o.classList.toggle('no');
+  p.classList.toggle('nko');
+  oo.classList.toggle('nkk');
+  pm.classList.toggle('ii');
+  oto.classList.toggle('iui');
+  
+}
+
+function uh(){
+  
+  var j = document.getElementById('color').value
+  
+  document.getElementById('card1').style.backgroundColor= j
+ 
+  document.getElementById('pd').style.display ="block"
+  
+  document.getElementById('hih').style.display ="none"
+  
+  document.getElementById('vlu').style.display ="block"
+  
+ document.getElementById("vlu").value=j;
+  
+}
+
+function rf(){
+  location.reload();
+}
+
+
+
